6708b19f22c6a94fde0c1a87e91e9d603e779804
md-mubeen <mdmubeen2112@gmail.com>
2024-02-19 23:50:02 +0530

    rte update

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,45 +1,30 @@
 import React from "react";
-import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
 
-export default function RTE({ name, control, label, defaultValue = "" }) {
+const RTE = ({ name, control, label, defaultValue = "" }) => {
   return (
-    <div className={"mb-6 w-full "}>
+    <div className="mb-6 w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
-
       <Controller
-        name={name || "content"}
+        name={name}
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
+            apiKey="r241v7trt06jfn4ckxf7qaxzj8niirga4qfa42sdi1eq6cqw"
             initialValue={defaultValue}
             init={{
-              initialValue: defaultValue,
               menubar: true,
               plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount",
               ],
               toolbar:
-                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                "undo redo | formatselect | " +
+                "bold italic backcolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
               content_style:
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
@@ -49,4 +34,6 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
       />
     </div>
   );
-}
+};
+
+export default RTE;
